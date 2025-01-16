a46b3b259e2aa005f355654b9707790bb212441a
Anusha N <nanu18071989@gmail.com>
2025-01-16 11:18:25 +0800

    loops2.sh

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[0m"   # RESET 
+N="\e[0m"   # RESET(\e represents escape character and [0m This is the ANSI escape code to reset all styles and colours)
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
