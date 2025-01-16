d3db7a9407eac799808946e0de8607c60de56d84
Anusha N <nanu18071989@gmail.com>
2025-01-16 11:38:34 +0800

    delete oldlogs

--- example.sh
+++ example.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo -e "First argument: $1"
+echo -e "First argument: $1"  
 echo -e "Second argument: $2"
+
+# (-e means enables interpretation of escape sequences in the string, such as \n(newline),\t(tab),\e(escap))
