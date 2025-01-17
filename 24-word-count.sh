38d3859b28b4cc0ee0757f56d5b74b857761df78
Anusha N <nanu18071989@gmail.com>
2025-01-17 11:50:45 +0800

    wordcount

--- 24-word-count.sh
+++ 24-word-count.sh
@@ -15,6 +15,13 @@ if [ ! -f "$FILE" ]; then
    echo "File not found: $FILE"
    exit 1
 fi
+# 1. Count occurences of each word
+# 2. Replace non-alphanumeric characters with spaces
+# 3. Split into words (one per line).
+# 4. Sort words alphabetically.
+# 5. Count occurences with 'uniq -c'.
+# 6. Sort by frequency in descending order
+# 7. Display the top 10 words.
 
 tr '[:upper:]' '[:lower:]' < "$FILE" | \
 tr -c '[:alnum:]' '[\n*]' | \ 
