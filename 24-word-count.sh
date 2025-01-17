0f35eb063c6af4cc70c483dd20755082549848c3
Anusha N <nanu18071989@gmail.com>
2025-01-17 11:51:28 +0800

    wordcount

--- 24-word-count.sh
+++ 24-word-count.sh
@@ -24,7 +24,7 @@ fi
 # 7. Display the top 10 words.
 
 tr '[:upper:]' '[:lower:]' < "$FILE" | \
-tr -c '[:alnum:]' '[\n*]' | \ 
+# tr -c '[:alnum:]' '[\n*]' | \ 
 sort | \
 uniq -c | \
 sort -nr | \
