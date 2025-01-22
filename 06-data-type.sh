b29bdf8cefe721e676a6dc54701837b693210ba7
Anusha N <nanu18071989@gmail.com>
2025-01-22 10:46:05 +0800

    palindrome

--- 06-data-type.sh
+++ 06-data-type.sh
@@ -8,3 +8,11 @@ echo "Script executed at: $TIMESTAMP"
 SUM=$(($NUMBER1 + $NUMBER2))
 
 echo "SUM of $NUMBER1 and $NUMBER2 is: $SUM"
+
+
+# (()) or expr have to use for arthimatic operations like sum, subtract, multiplication,division and modulus(finding remainder)
+
+# Logical operators and (&&),or(||), negate(!)
+# (&&)---> This operator is used to check if both conditions are true
+# (!!)---> This operator is used to check if atleast one condition is true
+# (!) --->This operator is used to negate a condition i.e., It checks if a condition is not true.
---
+++ 25-palindrome.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Function to check whether word is palindrome
+
+is_palindrome() {
+    local word="$1"
+    local reversed="${echo "$word" | rev}"
+    if [[ "$word" == "$reversed" ]]; then
+         echo "$word"
+
+    fi
+}
+
+# check if the file is provided and exists
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <path-to-text-file>"
+    exit 1
+fi
+
+if [[! -f "$file"]]; then
+    echo "File not found: $file"
+    exit 1
+fi
+
+# process the file to find palindromic words
+grep -oE '\b\w+\b' "$file" | tr ' [:upper:]' '[:lower:]' | while read -r word; do
+    is_palindrome "$word"
+done | sort -u     
+     
